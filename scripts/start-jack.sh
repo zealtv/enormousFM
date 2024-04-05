2830afc5ffa65ff8fb774595548474dab16f9c97
Bob Jarvis <bob@zeal.co>
2024-04-05 15:17:52 +1030

    updated jack script

--- scripts/start-jack.sh
+++ scripts/start-jack.sh
@@ -3,15 +3,20 @@
 #sudo service ntp stop
 sudo service triggerhappy stop
 sudo service dbus stop
+
 #not running on pi zero raspbian lite
 #sudo killall console-kit-daemon
 #sudo killall polkitd
 ## Only needed when Jack2 is compiled with D-Bus support
 #export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
-sudo mount -o remount,size=128M /dev/shm
+
+# the below was hanging on pi4 bookworm
+#sudo mount -o remount,size=128M /dev/shm
+
 #killall gvfsd
 #killall dbus-daemon
 #killall dbus-launch
+
 ## Uncomment if you'd like to disable the network adapter completely
 #echo -n "1-1.1:1.0" | sudo tee /sys/bus/usb/drivers/smsc95xx/unbind
 echo -n performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
