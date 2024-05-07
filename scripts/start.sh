f4fe95afe6d0c7695ff9c5bffea8d8575cf151b3
Bob Jarvis <bob@zeal.co>
2024-05-07 16:20:14 +1000

    starting python with sudo to enable networking (?)

--- scripts/start.sh
+++ scripts/start.sh
@@ -40,7 +40,7 @@ MACADDRESS=$(cat /sys/class/net/wlan0/address)
 echo "MAC: $MACADDRESS"
 
 # PYTHON
-/home/pi/venv/bin/python /home/pi/enormousFM/scripts/helper.py $MACADDRESS 
+sudo /home/pi/venv/bin/python /home/pi/enormousFM/scripts/helper.py $MACADDRESS 
 
 # PUREDATA
 pd -nogui -jack -open "/home/pi/enormousFM/pd/_MAIN.pd" -send "; RANDOM $RANDOM; " &
