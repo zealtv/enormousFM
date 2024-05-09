54a2b9881827f838e0d5223cf1bda427678299e4
Bob Jarvis <bob@zeal.co>
2024-05-09 12:21:26 +1000

    forward local osc messages to report

--- scripts/helper.py
+++ scripts/helper.py
@@ -38,20 +38,28 @@ def config_callback(path='', tags='', args='', source=''):
 def update_callback(path='', tags='', args='', source=''):
     directory = os.path.dirname(os.path.realpath(__file__))
     update_script = os.path.join(directory, "update.sh")
+    msg = OSCMessage("/update")
+    client.send(msg)
     print("UPDATE!")
     os.system(update_script)
 
 def updatesamples_callback(path='', tags='', args='', source=''):
     directory = os.path.dirname(os.path.realpath(__file__))
     update_script = os.path.join(directory, "updatesamples.sh")
+    msg = OSCMessage("/updatesamples")
+    client.send(msg)
     print("UPDATE SAMPLES!")
     os.system(update_script)        
 
 def shutdown_callback(path='', tags='', args='', source=''):
+    msg = OSCMessage("/shutdown")
+    client.send(msg)    
     print("SHUTDOWN!")
     os.system("systemctl poweroff")
 
 def reboot_callback(path='', tags='', args='', source=''):
+    msg = OSCMessage("/reboot")
+    client.send(msg)    
     print("REBOOTING")
     os.system("systemctl reboot")
 
